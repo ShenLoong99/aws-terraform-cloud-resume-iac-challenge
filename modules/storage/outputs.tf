8e11207f69d56781916d18869b66e884c9dd27ca
ShenLoong99 <yithuettsk29@gmail.com>
2026-04-25 20:07:02 +0800

    fix bugs

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -3,12 +3,12 @@ output "website_bucket_id" {
   value       = aws_s3_bucket.resume_bucket.id
 }
 
-output "domain_name" {
-  description = "Domain name of the S3 bucket"
-  value       = aws_s3_bucket.resume_bucket.bucket_regional_domain_name
-}
-
 output "s3_arn" {
   description = "ARN of the S3 bucket"
   value       = aws_s3_bucket.resume_bucket.arn
+}
+
+output "s3_bucket_domain" {
+  description = "The regional domain name of the S3 bucket (e.g., resume-bucket.s3.amazonaws.com)"
+  value       = aws_s3_bucket.resume_bucket.bucket_regional_domain_name
 }
