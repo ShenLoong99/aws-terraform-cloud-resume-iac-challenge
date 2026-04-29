4b8183e06cd08457be697ec5aab61839559941c5
ShenLoong99 <yithuettsk29@gmail.com>
2026-04-30 01:47:59 +0800

    remove create_before_destroy temporary

--- modules/cdn/acm.tf
+++ modules/cdn/acm.tf
@@ -21,10 +21,6 @@ resource "aws_acm_certificate" "cert" {
   domain_name               = var.domain_name
   subject_alternative_names = ["www.${var.domain_name}"]
   validation_method         = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 # This automatically creates the DNS record in Route 53
